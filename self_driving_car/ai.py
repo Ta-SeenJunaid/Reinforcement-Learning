9f0c65db13ccdd64bfb4cd8ce50ae1c3c730ebac
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-06-05 14:59:48 +0600

    init the Network class

--- self_driving_car/ai.py
+++ self_driving_car/ai.py
@@ -1,12 +1,24 @@
 
-#Importing libraries
+#importing libraries
 
 import numpy as np
 import random
 import os
 import tourch
-import torch.nn
+import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import tourch.autograd as autograd
 from tourch.autograd import Variable
+
+#architecture of nural network
+
+class Network(nn.Module):
+    
+    def __init__(self, input_size,nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.full_connection_1 = nn.Linear(input_size, 30)
+        self.full_connection_2 = nn.Linear(30, nb_action)
+
