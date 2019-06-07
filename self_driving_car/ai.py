35ed9d02313fcca498ae16ac9b710691ac5c390f
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-06-07 21:13:12 +0600

    update function from Dqn class is added

--- self_driving_car/ai.py
+++ self_driving_car/ai.py
@@ -76,6 +76,11 @@ class Dqn():
         self.optimizer.zero_grad()
         td_loss.backward(retain_variables = True)
         self.optimizer.step()
+        
+    def update(self, reward, new_signal):
+        new_state = torch.Tensor(new_signal).float().unsqueeze(0)
+        self.memory.push(self.last_state, new_state, torch.LongTensor([int(self.last_action)]),
+                          torch.Tensor([self.last_reward]))
     
     
         
